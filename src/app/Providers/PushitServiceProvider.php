bb706911887499147036fe37fcb0684f2dfd761f
kotsis <katskos@hotmail.com>
2018-09-30 01:00:29 +0300

    Fix service provider boot() add routes and configs

--- src/app/Providers/PushitServiceProvider.php
+++ src/app/Providers/PushitServiceProvider.php
@@ -22,6 +22,12 @@ class PushitServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->loadRoutesFrom(__DIR__.'../../routes/web.php');
+
+        //php artisan vendor:publish after installation of package...
+        $this->publishes([
+            __DIR__.'../../config/pushit.php' => config_path('pushit.php'),
+        ]);
     }
 
     /**
