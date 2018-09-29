da317067ac67d3ee59e0ab6761493db850728c4d
kotsis <katskos@hotmail.com>
2018-09-30 00:27:52 +0300

    Added a Laravel service provider and simple controller

---
+++ src/app/Http/Controllers/PushitController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Kotsis\Pushit\App\Http\Controllers;
+
+use App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class PushitController extends Controller
+{
+    //
+    public function index(){
+        return 'Hello World';
+    }
+}
+
