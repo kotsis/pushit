acf667102956085c2830c184cad71c526ff15695
Konstantinos Katsamakas <kmak@it.auth.gr>
2018-10-20 23:11:40 +0300

    vapid:init command test

--- src/app/Console/Commands/VapidInitCommand.php
+++ src/app/Console/Commands/VapidInitCommand.php
@@ -39,10 +39,31 @@ class VapidInitCommand extends Command
     {
         //To-do
         //Check if config/pushit.php exists, if not run vendor:publish .... command (php artisan vendor:publish --provider=Kotsis\Pushit\PushitServiceProvider)
+        Artisan::call('vendor:publish', [
+            '--provider' => 'Kotsis\Pushit\PushitServiceProvider'
+        ]);
+
         //Open pushit.php and check if values are present, if yes do nothing
         //If values not there create the VAPID key
         //openssl ecparam -name prime256v1 -genkey -noout -out vapid_private.pem
         //openssl ec -in vapid_private.pem -pubout -out vapid_public.pem
-        //done!
+        $config = array(
+            "private_key_type" => OPENSSL_KEYTYPE_EC,
+            'curve_name' => 'prime256v1'
+        );
+
+        // Create the private and public key
+        $res = openssl_pkey_new($config);
+
+        // Extract the private key from $res to $privKey
+        openssl_pkey_export($res, $privKey);
+
+        // Extract the public key from $res to $pubKey
+        $pubKey = openssl_pkey_get_details($res);
+        $pubKey = $pubKey["key"];
+        //Write key in pushit.php
+        //TO-DO
+        echo "Private:".$privKey."\n";
+        echo "Public:".$pubKey."\n";
     }
 }
